a56fa0c33e0b045fd634d3b1dbf094cc2e5ef68e
Pepe Valdivia <jvaldivia@softweb.pe>
2019-01-09 16:09:25 -0500

    Dibujando una línea

--- README.md
+++ README.md
@@ -14,14 +14,12 @@ En caso de usar el servicio en python:
     $ cd <<nombre_ambiente>>
     $ source bin/activate
 
-
-
-
 ### Fuentes:
 
 + https://www.pygame.org/wiki/GettingStarted
 + http://file.allitebooks.com/20180719/Instant%20Pygame%20for%20Python%20Game%20Development%20How-to.pdf
 + https://www.youtube.com/watch?v=xtk5lR150fg
++ https://www.youtube.com/watch?v=OQ4fJZa1-1M
 
 Thanks/Credits
 
--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import pygame
 from pygame.locals import *
 
 
-color = (0, 0, 255)
+color = (255, 255, 0) # rgb
 color2 = pygame.Color(0, 0, 255)
 
 pygame.init()
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hola mundo')
 
+window.fill(color2)
+pygame.draw.line(window, color, (60, 80), (160, 100), 8)
+
 while True:
-    window.fill(color)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
