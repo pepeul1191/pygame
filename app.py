44c71071425edca8ccbef1679043942c66e1d6f7
Pepe Valdivia <jvaldivia@softweb.pe>
2019-01-09 15:47:23 -0500

    Agregando colores rgb

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import pygame
 from pygame.locals import *
 
+
+color = (0, 0, 255)
+color2 = pygame.Color(0, 0, 255)
+
 pygame.init()
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Hola mundo')
 
 while True:
+    window.fill(color)
     for event in pygame.event.get():
-    	if event.type == QUIT:
-    		pygame.quit()
-    		sys.exit()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
     pygame.display.update()
